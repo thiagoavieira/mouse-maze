9fa9bf3895b270994a9c7524e77b111ebc840777
thiagoavieira <thiago.vmatos@hotmail.com>
2023-04-14 13:47:06 -0300

    Colisão adicionada

--- js/script.js
+++ js/script.js
@@ -3,19 +3,26 @@
     var cnv = document.querySelector("canvas");
     var ctx = cnv.getContext("2d");
 
+    // variaveis para limpar a tela
+    var WIDTH = cnv.width, HEIGHT = cnv.height;
+
     // variáveis para trabalhar com a movimentação do personagem na tela
     var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40; // códigos da seta do teclado
     var mvLeft = mvUp = mvRight = mvDown = false; // variáveis referentes a movi com valores booleanos
 
     // variavel para definir o tamanho dos blocos
-    var tileSize = 32; // 32px
+    var tileSize = 96; // 32px
+
+    // array com as paredes
+    var walls = [];
 
     // variaveis do player
     var player = {
         x: tileSize + 2, // 2px para ter uma distância
         y: tileSize + 2,
         width: 28, // largura de 28px
-        height: 28
+        height: 28,
+        speed: 2 // velocidade do boneco
     };
 
     // teremos um array com varios arrays, ou seja, um array de múltiplas dimensões (neste caso, 20x20)
@@ -43,6 +50,47 @@
 		[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     ];
     
+    for(var row in maze){ // p/ cada linha ↴
+        for(var col in maze[row]){  // pegar os índices da coluna
+            var tile = maze[row][col];
+            if(tile === 1){
+                var wall = { // declarar que vai ser um objeto com atributos
+                    x: tileSize * col, // tamanho dos bloquinhos * coluna
+                    y: tileSize * row,
+                    width: tileSize, // altura e largura vai ser do tamanho da célula
+                    height: tileSize 
+                };
+                // inserção do objeto no array de muros
+                walls.push(wall);
+            }
+        }
+    }
+
+    function blockRectangle(objA, objB){ // player e muro
+        // armazena a distancia dos objetos no eixo X -> este calculo retorna o ponto exato do ponto A no tabuleiro
+        var distX = (objA.x + objA.width / 2) - (objB.x + objB.width / 2);
+        // se estiver a direita, retorna um valor negativo, ao contrário, positivo, ou seja
+        // objeto B a esquerda do objeto A, positivo
+        var distY = (objA.y + objA.height / 2) - (objB.y + objB.height / 2);
+        // agora é preciso saber o valor da soma das metades da largura e da altura dos objetos
+        var sumWidth = (objA.width + objB.width) / 2;
+        var sumHeight = (objA.height + objB.height) / 2;
+        // agora é preciso saber se o valor absoluto da distância dos objetos em determinado eixo (X ou Y) é menor que
+        // a soma das larguras ou soma das alturas
+        if(Math.abs(distX) < sumWidth && Math.abs(distY) < sumHeight){ // se for verdade, colisão!!
+            // verifica quantos pixels o objA invadiu para ajustar, voltar na sua posição
+            var overlapX = sumWidth - Math.abs(distX);
+            var overlapY = sumHeight - Math.abs(distY);
+            // agora é preciso saber que lado ocorreu a colisão
+            if(overlapX > overlapY){ // a colisão ocorreu no eixo Y, ou o obj bateu de cima pra baixo ou de baixo pra cima
+                // se a dist em Y for maior que 0 isso quer dizer q o bloco estava acima do personagem
+                objA.y = distY > 0 ? objA.y + overlapY : objA.y - overlapY;
+            } else {
+                objA.x = distX > 0 ? objA.x + overlapX : objA.x - overlapX;
+            }
+        }
+    }
+
     window.addEventListener("keydown", keydownHandler, false); // evento para aguardar a interação de alguma tecla, que dispara uma função
     window.addEventListener("keyup", keyupHandler, false); // evento para quando soltar alguma tecla, que dispara uma função
     
@@ -84,11 +132,25 @@
 
     function update(){ // fun para atualizar os movimentos do jogo
         if(mvLeft && !mvRight){ // verificar o mvLeft se é VERDADEIRO e o mvRight armazenando FALSO, ou seja o personagem vai ser mover a ESQUERDA
+            player.x -= player.speed; // coordenada em x vai ser subtraida de x
+        } else if (mvRight && !mvLeft){ // caso esteja indo para direita
+            player.x += player.speed; //
+        }
+        if(mvUp && !mvDown){
+            player.y -= player.speed;
+        } else if (mvDown && !mvUp){
+            player.y += player.speed;
+        }
 
+        // verificar se o personagem está colidindo com as paredes
+        for(var i in walls){ // vai varrer todos os muros de walls
+            var wall = walls[i]; // recebendo o objeto que está sendo referenciado pelo indice i no array de muros
+            blockRectangle(player, wall); // pega um muro de cada vez e verifica se esta colidindo
         }
     } 
 
     function render(){ // fun para desenhar os elementos na tela
+        ctx.clearRect(0, 0, WIDTH, HEIGHT) // limpa a tela nas coordenadas 0 e 0, até a altura e largura total do canvas
         ctx.save(); // pego o contexto deste ponto e salvo na memória
         for(var row in maze){ // p/ cada linha ↴
             for(var col in maze[row]){  // pegar os índices da coluna
